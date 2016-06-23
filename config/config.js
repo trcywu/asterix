81c0217200e0a7a469fae6b31bc3bec5cb208756
Tracy Wu <sytracywu@gmail.com>
2016-06-23 15:15:04 +0100

    try 4 heroku

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  database: process.env.MONGOLAB_URI || "mongodb://localhost/asteroids-tracker",
+  database: process.env.MONGODB_URI || "mongodb://localhost/asteroids-tracker",
   secret: process.env.SECRET || "Acjhw338tw0@E*¢®^€∆sw2",
 };
